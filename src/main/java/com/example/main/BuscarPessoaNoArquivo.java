c36aa176787cec4c29815f38a0fedc3c920d64d7
guilh <guilhermedbdb@gmail.com>
2023-04-21 14:56:39 -0300

    Initial commit

---
+++ src/main/java/com/example/main/BuscarPessoaNoArquivo.java
@@ -0,0 +1,43 @@
+package com.example.main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Scanner;
+
+public class BuscarPessoaNoArquivo {
+
+    public static void buscaNome(File arquivo, String nomeBusca) throws IOException {
+        BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
+        String linha = leitor.readLine();
+        int numLinha = 1;
+        while (linha != null) {
+            if (linha.contains(nomeBusca)) {
+                System.out.println("Arquivo: " + arquivo.getName() + ", linha: " + numLinha + ", texto: " + linha);
+            }
+            linha = leitor.readLine();
+            numLinha++;
+        }
+        leitor.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome da pessoa que voce procura?");
+        String nomeBusca = scanner.nextLine();
+        File diretorio = new File("C:\\Users\\guilh\\Documents\\Faculdade\\comp paralela e distruida\\thread\\src\\dataset/");
+        File[] arquivos = diretorio.listFiles((dir, nome) -> nome.endsWith(".txt"));
+        Instant inicio = Instant.now();
+        for (File arquivo : arquivos) {
+            buscaNome(arquivo, nomeBusca);
+        }
+        Instant fim = Instant.now();
+        Duration duracao = Duration.between(inicio, fim);
+        System.out.println("Tempo total de execução: " + duracao.toMillis() + "ms");
+    }
+}
+
+
---
+++ src/main/java/multithread/BuscarPessoaNoArquivoComThreads.java
@@ -0,0 +1,68 @@
+package multithread;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BuscarPessoaNoArquivoComThreads {
+
+    private static final int NUM_THREADS = 4;
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o nome da pessoa que você procura:");
+        String nomeBusca = scanner.nextLine();
+        File diretorio = new File("C:\\Users\\guilh\\Documents\\Faculdade\\comp paralela e distruida\\thread\\src\\dataset/");
+        File[] arquivos = diretorio.listFiles((dir, nome) -> nome.endsWith(".txt"));
+        Instant inicio = Instant.now();
+
+        int numArquivosPorThread = (int) Math.ceil((double) arquivos.length / NUM_THREADS);
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < NUM_THREADS; i++) {
+            int inicioThread = i * numArquivosPorThread;
+            int fimThread = Math.min(inicioThread + numArquivosPorThread, arquivos.length);
+            Thread thread = new Thread(() -> {
+                for (int j = inicioThread; j < fimThread; j++) {
+                    try {
+                        buscaNome(arquivos[j], nomeBusca);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        Instant fim = Instant.now();
+        Duration duracao = Duration.between(inicio, fim);
+        System.out.println("Tempo total de execução: " + duracao.toMillis() + "ms");
+    }
+
+    public static void buscaNome(File arquivo, String nomeBusca) throws IOException {
+        BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
+        String linha = leitor.readLine();
+        int numLinha = 1;
+        while (linha != null) {
+            if (linha.contains(nomeBusca)) {
+                System.out.println("Arquivo: " + arquivo.getName() + ", linha: " + numLinha + ", texto: " + linha);
+            }
+            linha = leitor.readLine();
+            numLinha++;
+        }
+        leitor.close();
+    }
+}
