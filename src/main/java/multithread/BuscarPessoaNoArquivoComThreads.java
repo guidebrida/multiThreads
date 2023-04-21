ed65537fd63c5b43575402c914f56fed585f7319
guilh <guilhermedbdb@gmail.com>
2023-04-21 19:20:28 -0300

    Melhorias no codigo

--- src/main/java/multithread/BuscarPessoaNoArquivoComThreads.java
+++ src/main/java/multithread/BuscarPessoaNoArquivoComThreads.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 
 public class BuscarPessoaNoArquivoComThreads {
 
-    private static final int NUM_THREADS = 4;
+    private static final int NUM_THREADS = 2;
 
     public static void main(String[] args) throws InterruptedException, IOException {
         Scanner scanner = new Scanner(System.in);
@@ -27,21 +27,31 @@ public class BuscarPessoaNoArquivoComThreads {
 
         List<Thread> threads = new ArrayList<>();
 
-        for (int i = 0; i < NUM_THREADS; i++) {
-            int inicioThread = i * numArquivosPorThread;
-            int fimThread = Math.min(inicioThread + numArquivosPorThread, arquivos.length);
-            Thread thread = new Thread(() -> {
-                for (int j = inicioThread; j < fimThread; j++) {
-                    try {
-                        buscaNome(arquivos[j], nomeBusca);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        // Thread de busca de cima para baixo
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < numArquivosPorThread; i++) {
+                try {
+                    buscaNomeCimaBaixo(arquivos[i], nomeBusca);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            });
-            thread.start();
-            threads.add(thread);
-        }
+            }
+        });
+        thread1.start();
+        threads.add(thread1);
+
+        // Thread de busca de baixo para cima
+        Thread thread2 = new Thread(() -> {
+            for (int i = arquivos.length - 1; i >= arquivos.length - numArquivosPorThread; i--) {
+                try {
+                    buscaNomeBaixoCima(arquivos[i], nomeBusca);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread2.start();
+        threads.add(thread2);
 
         for (Thread thread : threads) {
             thread.join();
@@ -52,13 +62,14 @@ public class BuscarPessoaNoArquivoComThreads {
         System.out.println("Tempo total de execução: " + duracao.toMillis() + "ms");
     }
 
-    public static void buscaNome(File arquivo, String nomeBusca) throws IOException {
+    public static void buscaNomeCimaBaixo(File arquivo, String nomeBusca) throws IOException {
         BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
         String linha = leitor.readLine();
         int numLinha = 1;
         Boolean achouNome = false;
-        while (linha != null  &!achouNome) {
+        while (linha != null & !achouNome) {
             if (linha.contains(nomeBusca)) {
+                System.out.println("Thread 1");
                 System.out.println("Arquivo: " + arquivo.getName() + ", linha: " + numLinha + ", texto: " + linha);
                 achouNome = true;
             }
@@ -67,4 +78,23 @@ public class BuscarPessoaNoArquivoComThreads {
         }
         leitor.close();
     }
+
+    public static void buscaNomeBaixoCima(File arquivo, String nomeBusca) throws IOException {
+        BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
+        String linha = leitor.readLine();
+        int numLinha = 1;
+        Boolean achouNome = false;
+        while (linha != null & !achouNome) {
+            if (linha.contains(nomeBusca)) {
+                System.out.println("Thread 2");
+                System.out.println("Arquivo: " + arquivo.getName() + ", linha: " + numLinha + ", texto: " + linha);
+                achouNome = true;
+            }
+            linha = leitor.readLine();
+            numLinha++;
+        }
+        leitor.close();
+    }
+
+
 }
