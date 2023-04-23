a6d2bfd59e90917b31e9018f41d3fe3d815e9648
guilh <guilhermedbdb@gmail.com>
2023-04-23 10:54:38 -0300

    Melhorias no codigo

--- src/main/java/normal/BuscarPessoaNoArquivo.java
+++ src/main/java/normal/BuscarPessoaNoArquivo.java
@@ -9,16 +9,16 @@ import java.time.Instant;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import static oneThread.BuscarPessoaNoArquivoComUmaThread.nomeDaPessoa;
-import static oneThread.BuscarPessoaNoArquivoComUmaThread.tempoTotal;
+import static oneThread.BuscarPessoaNoArquivoComUmaThread.*;
 
 public class BuscarPessoaNoArquivo {
+
+    private static volatile boolean achouNome = false;
 
     public static void buscaNome(File arquivo, String nomeBusca) throws IOException, InterruptedException {
         BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
         String linha = leitor.readLine();
         int numLinha = 1;
-        Boolean achouNome = false;
         while (linha != null && !achouNome) {
             if (linha.contains(nomeBusca)) {
                 System.out.println("Arquivo: " + arquivo.getName() + ", linha: " + numLinha + ", texto: " + linha);//quando encontrar parar a busca
@@ -26,7 +26,7 @@ public class BuscarPessoaNoArquivo {
             }
             linha = leitor.readLine();
             numLinha++;
-            Thread.sleep(1);
+           Thread.sleep(1);
         }
         leitor.close();
     }
@@ -35,15 +35,20 @@ public class BuscarPessoaNoArquivo {
         Scanner scanner = new Scanner(System.in);
         System.out.println(nomeDaPessoa);
         String nomeBusca = scanner.nextLine();
-        File diretorio = new File("C:\\Users\\guilh\\Documents\\Faculdade\\comp paralela e distruida\\thread\\src\\dataset/");
+        File diretorio = new File(urlBase);
         File[] arquivos = diretorio.listFiles((dir, nome) -> nome.endsWith(".txt"));
         Arrays.sort(arquivos);
         Instant inicio = Instant.now();
         for (File arquivo : arquivos) {
-            buscaNome(arquivo, nomeBusca);
+            if (!achouNome){
+                buscaNome(arquivo, nomeBusca);
+            }
         }
         Instant fim = Instant.now();
         Duration duracao = Duration.between(inicio, fim);
+        if (!achouNome) {
+            System.out.println(arquivoNaoEncontrado);
+        }
         System.out.println(tempoTotal + duracao.getSeconds() + "s");
     }
 }
